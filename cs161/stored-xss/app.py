481f65fd8a6764afe0bcd82a9a8f4a5e9e67b390
John Yang <john.yang20@berkeley.edu>
2020-09-03 18:04:42 -0700

    [CS 161] Adding index route + templates

--- cs161/stored-xss/app.py
+++ cs161/stored-xss/app.py
@@ -4,7 +4,7 @@ import sqlite3
 
 from flask import Flask, redirect, render_template, request, url_for
 
-CREATE_QUERY = "CREATE table IF NOT EXISTS comment (id integer primary key, name TEXT, comment TEXT)"
+CREATE_QUERY = "CREATE table IF NOT EXISTS comments (id integer primary key, name TEXT, comment TEXT)"
 
 conn = sqlite3.connect('db.sqlite3')
 conn.execute(CREATE_QUERY)
@@ -12,9 +12,20 @@ conn.close()
 
 app = Flask(__name__)
 
+class Comment(object):
+    def __init__(self, name, comment):
+        self.name = name
+        self.comment = comment
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    comments = []
+    conn = sqlite3.connect('db.sqlite3')
+    c = conn.cursor()
+    for row in c.execute('SELECT name, comment FROM comments'):
+        comments.append(Comment(*row))
+    conn.close()
+    return render_template('index.html', posts=comments)
 
 if __name__ == "__main__":
     app.debug = True
