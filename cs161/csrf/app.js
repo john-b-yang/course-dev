5385fbf2cc4defd46c3e5601d6ca7509c9a66802
John Yang <john.yang20@berkeley.edu>
2020-09-07 14:49:12 -0700

    [CS 161] CSRF: Added html pages

--- cs161/csrf/app.js
+++ cs161/csrf/app.js
@@ -3,3 +3,9 @@ var express = require('express');
 
 // Initialize Express App
 var app = express();
+
+app.set('view engine', 'html')
+
+app.listen(3000, function() {
+    console.log('Server started successfully, listening at localhost:3000');
+})
